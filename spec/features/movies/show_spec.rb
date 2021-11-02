16df41e432e7f0a7f4623b43598cc445bffb01a5
Chaz Simons <chaz.simons@gmail.com>
2021-11-02 08:44:20 -0700

    Add Orderly gem to test order of cast

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -21,10 +21,17 @@ RSpec.describe 'Movie Show Page' do
 
   it 'displays a movies information' do
     visit movie_path(@raiders.id)
-    save_and_open_page
+
     expect(page).to have_content(@raiders.title)
     expect(page).to have_content(@raiders.genre)
     expect(page).to have_content(@raiders.creation_year)
     expect(page).to_not have_content(@starwars.title)
+  end
+
+  it 'displays the cast from youngest to oldest' do
+    visit movie_path(@raiders.id)
+
+    expect(@actor_3.name).to appear_before(@actor_1.name)
+    expect(@actor_4.name).to appear_before(@actor_1.name)
   end
 end
